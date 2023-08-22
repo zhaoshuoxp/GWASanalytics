f206967232b04f7f4b9a2a940f47c59b2c6e1d1a
Quanyi Zhao <quanyiz@stanford.edu>
2023-08-22 11:37:28 -0700

    Update Bed2GWASCatalog.sh

--- Bed2GWASCatalog.sh
+++ Bed2GWASCatalog.sh
@@ -41,7 +41,7 @@ all_catalog(){
 	echo "Intersecting overlaps"
 	for i in *.gwascatalog.bed;do
 		# make sure to use sort -u because extened peaks make have duplicated hits
-		intersectBed -a $i -b $1 |sort -u  > ${input}_${i/.gwascatalog.bed/}.overlap &
+		intersectBed -a $i -b $1 |sort -u  > ${pre}_${i/.gwascatalog.bed/}.overlap &
 	done
 	wait
 	echo "Intersecting done"
@@ -62,7 +62,7 @@ group_catalog(){
 	
 	echo "Intersecting overlaps"
 	for i in *.gwascatalog.bed;do
-		intersectBed -a $i -b $1 |sort -u  > ${input}_${i/.gwascatalog.bed/}.overlap &
+		intersectBed -a $i -b $1 |sort -u  > ${pre}_${i/.gwascatalog.bed/}.overlap &
 	done
 	echo "Intersecting done"
 }
@@ -125,7 +125,7 @@ main(){
 		echo "Running on grouped catalog by default"
 		# don't forget $1 to run
 		# take basename of $1 to keep output, otherwise errors when using input with path 
-		$input=$(basename $1)
+		pre=$(basename $1)
 		group_catalog $1
 		enrich $1
 		
@@ -163,7 +163,7 @@ main(){
 		all_catalog $1
 		enrich $1
 	fi
-	rm *.gwascatalog.bed ${input}_*.overlap input chromsize
+	rm *.gwascatalog.bed ${pre}_*.overlap input chromsize
 }
 
 main $1
